1b28f1c7336f81986eade8fd9ac3b50bd8678eaa
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-11-11 20:49:08 +0600

    context added

--- src/Layout/Main/Static/Home/Home.jsx
+++ src/Layout/Main/Static/Home/Home.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import MainContext from "../../../../Context/MainContext";
+
 const Home = () => {
+  const { name } = useContext(MainContext);
+  console.log(name);
   return <div>Home</div>;
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Router/Router.jsx";
+import { ToastContainer } from "react-toastify";
+import MainContextProvider from "./Context/MainContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router></Router>
+    <MainContextProvider>
+      <Router></Router>
+      <ToastContainer></ToastContainer>
+    </MainContextProvider>
   </StrictMode>
 );
